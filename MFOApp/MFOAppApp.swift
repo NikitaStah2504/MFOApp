24541a83fc06df3cd5d7953511e3ba9657724f73
Nikita <nikitastakhousky@gmail.com>
2023-06-19 16:30:02 +0300

    Initial Commit

---
+++ MFOApp/MFOAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MFOAppApp.swift
+//  MFOApp
+//
+//  Created by Nikita Stshovsky on 19.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct MFOAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
