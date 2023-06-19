f95fe13ea94a764b54e5ca89e5557919da63b89d
Nikit <nikita.s@olimob.com>
2023-06-19 20:58:18 +0200

    FinalFix

--- OneSignalNotificationServiceExtension/NotificationService.swift
+++ OneSignalNotificationServiceExtension/NotificationService.swift
@@ -6,18 +6,22 @@
 //
 
 import UserNotifications
+import OneSignal
 
 class NotificationService: UNNotificationServiceExtension {
 
+    var receivedRequest: UNNotificationRequest!
     var contentHandler: ((UNNotificationContent) -> Void)?
     var bestAttemptContent: UNMutableNotificationContent?
 
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        self.receivedRequest = request
         self.contentHandler = contentHandler
         bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
         
         if let bestAttemptContent = bestAttemptContent {
-            // Modify the notification content here...
+            OneSignal.didReceiveNotificationExtensionRequest(self.receivedRequest,with:bestAttemptContent,
+            withContentHandler: self.contentHandler)
             bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
             
             contentHandler(bestAttemptContent)
@@ -28,6 +32,7 @@ class NotificationService: UNNotificationServiceExtension {
         // Called just before the extension will be terminated by the system.
         // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
         if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+            OneSignal.serviceExtensionTimeWillExpireRequest(self.receivedRequest, with: self.bestAttemptContent)
             contentHandler(bestAttemptContent)
         }
     }
